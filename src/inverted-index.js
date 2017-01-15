fd62bec241f6d849b86738231e02c3da6090fd91
andela-nrotimi <rotimi.nelson@andela.com>
2017-01-15 05:26:19 +0100

    validate uploaded file

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -4,16 +4,24 @@
 
 class InvertedIndex {
 
-  static getFile() {
-    alert('yoooo!');
-    // let  extn = filename.split(".").pop();
+/**
+ * A function to validate uploaded file
+ */
+  static validateFile() {
+    let filename = document.getElementById("filename_id").value;
+    let extension = filename.split(".").pop();
+    document.getElementById("uploadinfo_panel").removeAttribute("hidden");
+    let uploadinfo_id = document.getElementById("uploadinfo_id");
+    if (extension === "json") {
+      uploadinfo_id.innerHTML="Your file uploaded successfully !"
+      uploadinfo_id.className = "green-text text-darken-3";
+    } else {
+      uploadinfo_id.innerHTML="Ops! Invalid file detected!"
+      uploadinfo_id.className = "red-text text-darken-3";
+    }
   }
 
-  validateFile() {
-
-  }
-
-  readFile () {
+  readFile() {
 
   }
 

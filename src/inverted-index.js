81030d79a5de46dbb5603bff12bc2e041b190759
andela-nrotimi <rotimi.nelson@andela.com>
2017-01-16 05:36:20 +0100

    convert array to string and remove multiple words

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -3,6 +3,12 @@
  */
 class InvertedIndex {
   /**
+   * Constructor to initialise
+   */
+  constructor() {
+    this.texter = '';
+  }
+  /**
    * A function to validate uploaded file
    */
   static validateFile() {
@@ -13,37 +19,56 @@ class InvertedIndex {
     if (extension === 'json') {
       uploadInfoId.innerHTML = 'File uploaded successfully !';
       uploadInfoId.className = 'green-text text-darken-3';
-      console.log('if block.....');
       InvertedIndex.readFile();
     } else {
-      uploadInfoId.innerHTML = 'Ops! Invalid file Input detected!';
+      uploadInfoId.innerHTML = 'Oops! Invalid file Input detected!';
       uploadInfoId.className = 'red-text text-darken-3';
-      console.log('else block.....');
     }
   }
   /**
    * A function readFile to read into
-   * the content of the json files
+   * the content of the json file
    */
   static readFile() {
-    console.log('am in read file');
-    jQuery.getJSON('jasmine/books.json', function (doc) {
-      console.log('inside jquery scope');
-      let docCounter = doc.length;
-      //console.log(docCounter);
+    jQuery.getJSON('jasmine/books.json', (doc) => {
+
+      const docCounter = doc.length;
       if (docCounter === 0) {
         alert('No json object found !');
       } else {
-        let title = [];
-        let text = [];
-        console.log(docCounter);
+        const title = [];
+        const text = [];
+
         for (let i = 0; i < docCounter; i++) {
           title.push(doc[i].title);
           text.push(doc[i].text);
         }
-        console.log(title);
-        console.log(text);
+        InvertedIndex.stringifyArray(title, text);
       }
     });
+  }
+  /**
+   * Function stringifyArray to convert array
+   * into corresponding text and also remove
+   * special characters from the text
+   * @param: title
+   */
+  static stringifyArray(title, text) {
+    let rawText = '';
+    for (let j = 0; j < text.length; j++) {
+      rawText = rawText + text[j];
+    }
+    let filteredText = rawText.replace(/[^a-z\d\s]+/gi, ' ');
+    console.log(filteredText);
+    InvertedIndex.removeMultipleWords(filteredText);
+  }
+  /**
+   * Function removeMultipleWords to
+   * remove multiple occurrences of
+   * words
+   */
+  static removeMultipleWords(filteredText) {
+    let uniqueList = filteredText.split(' ').filter((item, i, allItems) => i === allItems.indexOf(item)).join(',');
+    console.log(uniqueList);
   }
 }
